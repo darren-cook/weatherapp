56e170ebe79594ec76bbddcba358b7dc89952003
darren-cook <darren.j.cook@uconn.edu>
2022-05-29 18:18:55 -0400

    testing no localstorage and on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { initial } from "./modules/initial";
 import { handleSearch } from "./modules/weatherSearch";
+import { getAndDisplayWeather } from "./modules/weatherData";
 
-initial();
+const locationObject = {
+    city:"Los Angeles",
+    coordinates:["34.0522", "-118.2436"] 
+};
+
+getAndDisplayWeather(locationObject);
+
+// initial();
 
 const inputSearch = document.getElementById("input-search");
 
